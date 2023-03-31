461bed9bc863cb9f454468d672f833841476e0a8
KristoferGab <kristofer.gabrielsson@gmail.com>
2023-03-31 02:51:47 +0000

    Collect data for last 5 markets in list of columns

--- run.py
+++ run.py
@@ -80,6 +80,17 @@ def calculate_surlpus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """docstrig"""
+    sales = SHEET.worksheet(('sales'))
+
+    columns = []
+    for i in range(1, 7):
+        column = sales.col_values(i)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     Run all program function
@@ -92,4 +103,5 @@ def main():
 
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
